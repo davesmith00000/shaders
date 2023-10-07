fc7ee1f6034c8adaa3818cdfe935672df77c1fe5
Dave Smith <davesmith.00000@gmail.com>
2023-10-07 13:02:30 +0100

    Fixed coloured metaballs

--- shaders/shapes/coloured-metaballs/src/ColouredMetaballs.scala
+++ shaders/shapes/coloured-metaballs/src/ColouredMetaballs.scala
@@ -40,27 +40,29 @@ object CustomShader:
       def fragment(color: vec4): vec4 =
         val uv: vec2 = (2.0f * env.SCREEN_COORDS - env.SIZE) / env.SIZE.y
 
-        var accColor: vec3 = vec3(0.0f)
+        var accColor: vec3  = vec3(0.0f)
         var accAlpha: Float = 0.0f
-        val count      = 20.0f
-        val red = vec3(1.0f, 0.0f, 0.0f)
-        val blue = vec3(0.0f, 0.0f, 1.0f)
+        val count           = 20.0f
+        val red             = vec3(1.0f, 0.0f, 0.0f)
+        val blue            = vec3(0.0f, 0.0f, 1.0f)
 
         _for(1.0f, _ < count, _ + 1.0f) { i =>
           val start        = N22(vec2(i))
           val p: vec2      = sin(start * env.TIME)
           val distance     = length(uv - p)
-          val blobStrength = 10.0f
-          val radius       = 0.1f
+          val blobStrength = 100.0f
+          val radius       = 0.08f
           val influence    = blobStrength * exp(-distance * distance / (2.0f * radius * radius))
-          
+
           val c = mix(red, blue, step(0.6f, fract(start.x)))
 
-          accColor = accColor + ((c * influence) / count)
+          accColor = accColor + (c * influence)
           accAlpha = clamp(accAlpha + (influence / count), 0.0f, 1.0f)
         }
 
-        val col = vec4(accColor * accAlpha, accAlpha)
-        smoothstep(col, vec4(0.0f), vec4(0.05f))
+        val newAlpha = step(0.5f, accAlpha)
+        val newColor = accColor / (accColor.r + accColor.g + accColor.b)
+
+        vec4(newColor * newAlpha, newAlpha)
 
     }
